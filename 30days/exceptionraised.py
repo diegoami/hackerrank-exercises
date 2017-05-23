e117cb354b68a2e8ec4d00116a8fea20c69efff9
Diego Amicabile <diego.amicabile@gmail.com>
2017-05-24 00:58:13 +0200

    Other problems

---
+++ 30days/exceptionraised.py
@@ -0,0 +1,39 @@
+state = 0
+inputarray = [
+"10",
+"10 0",
+"0 10",
+"-1 4",
+"2 -3",
+"-2 -2",
+"5 6",
+"3 3",
+"8 0",
+"2 3",
+"3 -3"
+]
+
+def input():
+    global state
+    result = inputarray[state]
+    state += 1
+    return result
+
+
+import math
+class Calculator:
+    def power(self,n,p):
+        if n >= 0 and p >= 0:
+            return int(n**p)
+        else:
+            raise(Exception("n and p should be non-negative"))
+
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e)
