831ba922d5209ed4423a120cffd246fd8e487c9a
Diego Amicabile <diego.amicabile@gmail.com>
2017-05-25 00:24:29 +0200

    Graphs at last...

--- tree/roads_and_libraries.py
+++ tree/roads_and_libraries.py
@@ -2,49 +2,38 @@ state = 1
 import linecache
 def input():
     global state
-    result = linecache.getline('testcase_1.txt', state)
+    result = linecache.getline('testcase_4.txt', state)
     state += 1
     return result
 
 from collections import deque
 class AdjMatrix:
-    def __init__(self,n,m,cl,cr):
-        self.A = []
-        self.C = {}
-        for i in range(n):
-            Ai = []
-            self.A.append(Ai)
-            for j in range(n):
-                self.A[i].append(0)
 
+    def __init__(self,n,m,cl,cr):
+        self.A = {k:[] for k in range(1,n+1)}
         self.cr = cr
         self.cl = cl
+        self.n  = n
+        self.VUS = []
+
+    def get_connections(self,v):
+        return self.A[v]
 
     def add_city(self,city_1,city_2):
-        self.A[city_1][city_2] = 1
-        self.A[city_2][city_1] = 1
-
-    def get_adjacent(self,n):
-        return self.a[n]
-
-    def graph(self):
-        return self.A
-
-    def conn(self):
-        return [x[1] for x in self.C.items() if len(x[1]) > 1]
+        self.A[city_1].append(city_2)
+        self.A[city_2].append(city_1)
 
     def cost(self):
         if self.cr >= self.cl:
-            return len(self.A)*self.cl
+            return n*self.cl
         else:
             tl, tr =self.walk_graph()
             return tl*self.cl + tr*self.cr
 
-
     def walk_graph(self):
         GVU = set()
         tl, tr = 0, 0
-        for i in range(len(self.A)):
+        for i in range(1,n+1):
             if i in GVU:
                 continue
             tl += 1
@@ -53,13 +42,11 @@ class AdjMatrix:
             S.append(i)
             while (len(S) > 0):
                 v = S.pop()
+
                 if v not in VU and v not in GVU:
                     VU.add(v)
                     GVU.add(v)
-                    for j,Aj in enumerate(self.A[v]):
-                        if Aj > 0 and v != j:
-                            S.append(j)
-
+                    S.extend(self.get_connections(v))
             tr += len(VU)-1
         return (tl,tr)
 
@@ -71,7 +58,7 @@ for a0 in range(q):
     for a1 in range(m):
         city_1,city_2 = input().strip().split(' ')
         city_1,city_2 = [int(city_1),int(city_2)]
-        Aj.add_city(city_1-1,city_2-1)
+        Aj.add_city(city_1,city_2)
 
     print(Aj.cost())
 
