fe39743979c02d69432a007084b0ace17f2bd0be
Diego Amicabile <diego.amicabile@gmail.com>
2017-05-30 02:48:51 +0200

    Insertion sort, bread first tree

---
+++ 30days/bst_2.py
@@ -0,0 +1,58 @@
+state = 0
+inputarray = [
+"6",
+"3",
+"5",
+"4",
+"7",
+"2",
+"1"
+]
+
+def input():
+    global state
+    result = inputarray[state]
+    state += 1
+    return result
+
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        from collections import deque
+        V = set()
+        Q = deque()
+        Q.append(root)
+        while len(Q) > 0:
+            c = Q.popleft()
+            V.add(c)
+            print(c.data,end=' ')
+            if c.left:
+                Q.append(c.left)
+            if c.right:
+                Q.append(c.right)
+
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
---
+++ sort/inssort_2.py
@@ -0,0 +1,42 @@
+state = 0
+
+inputarray = [
+"5",
+"2 4 6 8 3"
+]
+
+inputarray2 = [
+"6",
+"1 4 3 5 6 2"
+]
+
+
+def input():
+    global state
+    result = inputarray2[state]
+    state += 1
+    return result
+
+def insertion_sort(m,e):
+    #print(m,e)
+    l = list(m)
+    index = len(l)-1
+    while any([l[i] > e for i in range(index+1)] ) and index >= 0:
+        l = (m[:index]+[m[index],m[index]]+m[index+1:])
+
+        index -= 1
+        #print(l,index)
+    l = (m[:index+1]+[e]+m[index+1:])
+    #print(l)
+    return l
+
+
+m = int(input().strip())
+ar = [int(i) for i in input().strip().split()]
+
+r = ar
+for i in range(1,len(ar)):
+    e = ar[i]
+    if (any([r[j] > e for j in range(i+1)])):
+        r = insertion_sort(r[:i]+r[i+1:], e)
+    print(" ".join(map(str, r)))
