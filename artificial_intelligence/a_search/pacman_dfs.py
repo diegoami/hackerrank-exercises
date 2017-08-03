4c8760c1d5bafdd94c11a331b5f4b8c0910746d4
diegoami <diego.amicabile@gmail.com>
2017-08-03 18:48:14 +0200

    Exercise completed

--- artificial_intelligence/a_search/pacman_dfs.py
+++ artificial_intelligence/a_search/pacman_dfs.py
@@ -97,14 +97,14 @@ def walk(maze, sy,sx, y,x):
     max_loops = 500
 
 
-    addedPath, exploredPath = [], []
+    exploredPath =  []
     while len(Q) > 0 and max_loops > 0:
         max_loops -= 1
         (cy,cx) = Q.pop()
         exploredPath.append((cy,cx))
 
         if ((cy,cx) == (y,x)):
-            return (exploredPath, addedPath)
+            return exploredPath
         else:
             cand_dirs = [(cy-1,cx),(cy,cx-1),(cy,cx+1),(cy+1,cx)]
             poss_dir = [(j,i) for (j,i) in cand_dirs if 0 < j < len(maze) and  0 < i < len(maze[0]) ]
@@ -113,15 +113,21 @@ def walk(maze, sy,sx, y,x):
                 if (mzchr != '%' ):
                     if (j,i) not in S:
                         S.add((j,i))
-                        #s_dirs = [(j+1,x),(j,x+1),(j,x-1),(j-1,x)]
-                        #for s_dir in s_dirs:
-                        #    if addedPath.index(s_dir) > 0:
-
-                        addedPath.append((j, i))
-
                         Q.append((j,i))
 
+def clean_path(explored_path):
+    shortest_path = []
+    for index,(cy,cx) in enumerate(explored_path):
+        s_dirs = [(cy + 1, cx), (cy, cx + 1), (cy, cx - 1), (cy - 1, cx)]
+        if len(shortest_path) > 0:
+            py, px = shortest_path[-1]
+            s_dirs = [(cy,cx) for (cy,cx) in s_dirs if (cy,cx) != (py,px)]
+        for s_dir in s_dirs:
+            if s_dir in shortest_path:
+                shortest_path =  shortest_path[:shortest_path.index(s_dir)+1]
+        shortest_path.append((cy,cx))
 
+    return shortest_path
 if __name__ == "__main__":
 
     maze = []
@@ -133,11 +139,11 @@ if __name__ == "__main__":
     for _ in range(ly):
         maze.append(input())
 
-    exploredPath, addedPath = walk(maze,sy,sx,y,x)
+    exploredPath = walk(maze,sy,sx,y,x)
     print(len(exploredPath))
     for p in exploredPath:
         print(*p, sep=" ")
-
+    addedPath = clean_path(exploredPath)
     print(len(addedPath)-1)
     for p in addedPath:
         print(*p, sep=" ")
