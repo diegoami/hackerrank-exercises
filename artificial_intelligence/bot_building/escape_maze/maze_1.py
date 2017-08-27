fecfe917cdcf36c1c728bab9060efd7e3ea02549
diegoami <diego.amicabile@gmail.com>
2017-08-27 17:06:12 +0200

    Boring maze

--- artificial_intelligence/bot_building/escape_maze/maze_1.py
+++ artificial_intelligence/bot_building/escape_maze/maze_1.py
@@ -8,7 +8,7 @@ def find_exit(maze):
             return (j,maze[j].index('e'))
     return None
 
-dirs = ['UP','RIGHT','DOWN','LEFT']
+dirs = ['UP','LEFT','DOWN','RIGHT']
 def turn_dir(orig_dir, prev_dir):
     prev_dir_index = dirs.index(prev_dir)
     orig_dir_index = dirs.index(orig_dir)
@@ -17,6 +17,16 @@ def turn_dir(orig_dir, prev_dir):
         prev_dir_index -= 1
         if orig_dir_index == -1:
             orig_dir_index = 3
+    return dirs[orig_dir_index]
+
+def turn_facing_dir(orig_dir, prev_dir):
+    prev_dir_index = dirs.index(prev_dir)
+    orig_dir_index = dirs.index(orig_dir)
+    while prev_dir_index > 0:
+        orig_dir_index += 1
+        prev_dir_index -= 1
+        if orig_dir_index == 4:
+            orig_dir_index = 0
     return dirs[orig_dir_index]
 
 def turn_maze(maze , prev_dir):
@@ -35,30 +45,33 @@ def turn_maze(maze , prev_dir):
 
 
 
-def get_best_move(maze, dir="UP"):
+def get_best_move(maze, prev_dir = "UP", prev_norm_dir= "UP"):
 
     def walk_possible(maze, dir):
-        if dir == 'DOWN' and maze[2][1] == '-':
+        if dir == 'DOWN' and maze[2][1] in ['-','e']:
             return True
-        elif dir == 'RIGHT' and maze[1][2] == '-':
+        elif dir == 'RIGHT' and maze[1][2] in ['-','e']:
             return True
-        elif dir == 'UP' and maze[0][1] == '-':
+        elif dir == 'UP' and maze[0][1] in ['-','e']:
             return True
-        elif dir == 'LEFT' and maze[1][0] == '-':
+        elif dir == 'LEFT' and maze[1][0] in ['-','e']:
             return True
 
 
     exit = find_exit(maze)
     if (exit):
-        if exit[0] == 2:
-            return 'DOWN'
-        elif exit[0] == 0:
+        if exit[0] == 0 and walk_possible(maze,'UP'):
             return 'UP'
-        elif exit[1] == 0:
+        elif exit[1] == 2 and walk_possible(maze,'RIGHT')  :
+            return 'RIGHT'
+        elif exit[1] == 0 and walk_possible(maze,'LEFT') :
             return 'LEFT'
         else:
-            return 'RIGHT'
+            return 'DOWN'
+
     else:
+        dir = prev_dir
+
         while True:
             if walk_possible(maze,dir):
                 return dir
@@ -93,20 +106,22 @@ def process(input):
         mazej = input()
         for i in range(3):
             maze[j].append(mazej[i])
-    prev_dir, prev_norm_dir = read_direction()
-    print("======PREV_DIR === {} == {} =======".format(prev_dir,prev_norm_dir), file=sys.stderr)
+    prev_dir, facing_dir = read_direction()
+    print("==== PREV_DIR == {} === FACING_DIR {} =======".format(prev_dir,facing_dir ), file=sys.stderr)
 
 
-    new_maze = turn_maze(maze,prev_norm_dir)
+    #new_maze = turn_maze(maze,prev_norm_dir)
     #print(maze)
-    print("======NORMAL MAZE === ", file=sys.stderr)
-    print(new_maze,file=sys.stderr)
+    print("===== MAZE === ", file=sys.stderr)
+    print(maze,file=sys.stderr)
 
-    orig_dir = get_best_move(new_maze,prev_norm_dir)
-    new_dir = turn_dir(orig_dir, prev_dir)
-    print("======NEW DIR === {} == {}== ".format(new_dir, orig_dir), file=sys.stderr)
+    orig_dir = get_best_move(maze)
+    #new_dir = turn_dir(orig_dir, prev_dir)
+    new_dir = orig_dir
+    new_facing_dir = turn_facing_dir(new_dir, new_dir)
+    print("====== DIR === {} ==== NEW_FACING_DIR == {} == ".format(new_dir, new_facing_dir), file=sys.stderr)
 
-    write_direction(new_dir, orig_dir)
+    write_direction(new_dir, new_facing_dir )
     print(new_dir)
 
 def do_test_inputs():
